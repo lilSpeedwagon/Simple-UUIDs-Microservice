6a13af01db9390f9896fa438d4387238ccd5ee17
lilSpeedwagon <ded_eg0r@mail.ru>
2020-08-21 22:02:31 +0300

    routing, requests handling

--- index.js
+++ index.js
@@ -1,20 +1,170 @@
-const log = require('./log');
-const http = require('http');
-const express = require('express');
-
-const port = 8080;
-const app = express();
+const log       = require("./log");
+const express   = require("express");
+const uuid      = require("uuid");
 
 
+// server consts
+const STATUS_OK         = 200;
+const STATUS_BAD        = 503;
+const STATUS_NOT_FOUND  = 404;
+const STATUS_ERROR      = 500;
+
+const ERR_BAD_REQUEST   = "Bad request";
+
+const PORT = 8080;
+
+const UUID_NULL = "00000000-0000-0000-0000-000000000000";
+const UUIDTypeEnum = { v3: "v3", v5: "v5" };
+
+
+// metrics variables
+var nUUIDv3Generated    = 0;
+var nUUIDv5Generated    = 0;
+var nBadResponces       = 0;
+var nBadRequests        = 0;
+
+
+// server instance
+var app = express();
+
+function isServerOK()   {
+    return true;
+}
+
+function sendStatus(response, status)   {
+    log.log("Response status: " + status + ".");
+    response.sendStatus(status);
+}
+
+function getRequestVariable(request, varName)   {
+    let value = request.query[varName];
+    if (value === undefined) {
+        value = "";
+    }
+    return value;
+}
+
+
+// request preprocessing
 app.use((request, response, next) =>    {
     log.log("Incoming request \"" + request.url + "\".");
     next();
 });
 
-app.listen(port, (error) => {
+
+// routing
+const REQUEST_V3        = "/v3";
+const REQUEST_V5        = "/v5";
+const REQUEST_HEALTH    = "/health";
+const REQUEST_METRICS   = "/metrics";
+const REQUEST_ANY       = "*";
+
+app.get(REQUEST_V3, (request, response, next) => {
+    log.log("UUIDv3 request");
+    
+    let name = getRequestVariable(request, "value");
+
+    let uuidv3 = generateUUID(name, UUIDTypeEnum.v3);
+    response.send(uuidv3);
+});
+
+app.get(REQUEST_V5, (request, response, next) => {
+    log.log("UUIDv5 request");
+
+    let name = getRequestVariable(request, "value");
+
+    let uuidv5 = generateUUID(request.query.value, UUIDTypeEnum.v5);
+    response.send(uuidv5);
+});
+
+app.get(REQUEST_HEALTH, (request, response, next) => {
+    log.log("Health check request.");
+    sendStatus(response, getServerStatus());
+});
+
+app.get(REQUEST_METRICS, (request, response, next) => {
+    log.log("Metrics request.");
+    response.send(getMetrics());
+});
+
+app.get(REQUEST_ANY, (request, response, next) => {
+    nBadRequests++;
+    throw new Error(ERR_BAD_REQUEST);
+});
+
+
+// error handling
+app.use((error, request, response, next) => {
+    log.err(error.message);
+
+    let status = STATUS_ERROR;
+    if (error.message === ERR_BAD_REQUEST) {
+        status = STATUS_NOT_FOUND;
+    }
+
+    sendStatus(response, status);
+    next();
+});
+
+
+// start listening
+app.listen(PORT, (error) => {
     if (error)    {
         return log.err(error);
     }
 
-    log.log("server is listening on port " + port + "...");
+    log.log("Server is listening on port " + PORT + "...");
 });
+
+
+// internal logic
+function getServerStatus()  {
+    let status = isServerOK() ? STATUS_OK : STATUS_BAD;
+    if (status == STATUS_BAD)   {
+        nBadResponces++;
+    }
+
+    return status;
+}
+
+function generateUUID(name, type)   {
+    let base = process.env.BASE_UUID;
+    if (base === undefined) {
+        log.warn("BASE_UUID env variable is undefined. Setting base uuid as null UUID.");
+        base = UUID_NULL;
+    }
+
+    let resultUUID;
+    if (type === UUIDTypeEnum.v3)   {
+        resultUUID = uuid.v3(name, base);
+        log.log("Generated UUIDv3 is " + resultUUID + ".");
+        nUUIDv3Generated++;
+    }
+    else if (type === UUIDTypeEnum.v5)  {
+        resultUUID = uuid.v5(name, base);
+        log.log("Generated UUIDv5 is " + resultUUID + ".");
+        nUUIDv5Generated++;
+    }
+    else    {
+        throw new Error("Undefined UUID type.");
+    }
+
+    return resultUUID;
+}
+
+function getMetrics()   {
+    let getCounterText = (name, value, desc) =>   {
+        return  "# HELP " + name + " " + desc + "\n" +
+                "# TYPE " + name + " counter\n" +
+                name + " " + value + " " + Date.now() + "\n\n";
+    };
+
+    let metricsText = "";
+
+    metricsText += getCounterText("uuidv3_count", nUUIDv3Generated, "Number of generated UUIDs for v3.");
+    metricsText += getCounterText("uuidv5_count", nUUIDv5Generated, "Number of generated UUIDs for v5.");
+    metricsText += getCounterText("bad_responce_count", nBadResponces, "Number of 503 responses.");
+    metricsText += getCounterText("bad_request_count", nBadRequests, "Number of requests to unsupported URIs (404 responses).");
+
+    return metricsText;
+}
--- log/log.js
+++ log/log.js
@@ -5,44 +5,59 @@ module.exports.err = err;
 
 // export functions
 function log(message)   {
-    internalLog("INFO", message);
+    internalLog(LVL_LOG, message);
 }
 
 function warn(message) {
-    internalLog("WARNING", message);
+    internalLog(LVL_WARN, message);
 }
 
 function err(message)  {
-    internalLog("ERROR", message);
+    internalLog(LVL_ERR, message);
 }
 
 
-// internal variables
+// internal variables and consts
 const fileSystem = require('fs')
-var logFileName = "log.txt";
+
+const FILE_NAME_DEFAULT = "log.txt";
+
+const LVL_LOG   = "INFO";
+const LVL_WARN  = "WARNING";
+const LVL_ERR   = "ERROR";
 
 
 // internal functions
 function internalLog(level, message)   {
-    let fullMessage = level + "\t " + getTimeStamp() + ":\t " + message + "\n";
+    let fullMessage = getFullMessage(level, message);
 
     console.log(fullMessage);
     writeToLogFile(fullMessage);
 }
 
 function writeToLogFile(message)   {
-    fStream = fileSystem.createWriteStream(getLogFileName(), {flags: 'w'});
-    fStream.write(message);
+    fStream = fileSystem.createWriteStream(getLogFileName(), {flags: 'a'});
+    fStream.on("error", (e) => {
+        let msg = getFullMessage(LVL_WARN, "Cannot write log to " + e.path + ".");
+        console.warn(msg);
+    });
+
+    fStream.write(message + '\n');
+}
+
+function getFullMessage(level, message) {
+    return fullMessage = level + "\t " + getTimeStamp() + ":\t " + message;
 }
 
 function getLogFileName()   {
-    // read from env variable LOG_PATH
-    // if (logFileName == "")   {
-    //         
-    // }
+    let logFileName = process.env.LOG_PATH;
+    if (logFileName === undefined)  {
+        logFileName = FILE_NAME_DEFAULT;
+    }
+    
     return logFileName;
 }
 
 function getTimeStamp() {
-    return new Date().toISOString();
+    return new Date().toLocaleString();
 }
